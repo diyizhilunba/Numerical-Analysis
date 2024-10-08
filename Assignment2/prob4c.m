354835b7c39f1b5efa2b82c5d6d168a3fbe20c67
diyizhilunba <mayilun811@gmail.com>
2024-10-07 21:33:54 -0400

    Update prob4c.m

--- Assignment2/prob4c.m
+++ Assignment2/prob4c.m
@@ -1,6 +1,6 @@
 addpath Assignment\Assignment2\functions\
 
-A = [-2,-1,0;
+A = [2,-1,0;
     -1,2,-1;
     0,-1,2];
 b = [1;
@@ -10,9 +10,14 @@ b = [1;
 [L,U,~] = lu_nopivot(A,b);
 y = forwardsub(L,b);
 x = backsub(U,y);
+disp("L")
+disp(L)
+disp("U")
+disp(U)
+disp("y")
+disp(y)
 disp("The solution by LU factorization is")
 disp(x)
-disp("The actual solution is")
-disp(A\b)
+
 disp("The error in the Euclidean norm")
-disp(norm(x-A\b));
+disp(norm(x-[1;1;1]));
